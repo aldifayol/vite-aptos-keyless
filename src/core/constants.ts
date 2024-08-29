075c978d4552f9b15d5d963e9bff277e3eeb9fa8
Aldi <aldifayol@gmail.com>
2024-08-29 20:59:08 +0700

    FEAT: initial commit to gh

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -7,9 +7,8 @@ export const LocalStorageKeys = {
   keylessAccounts: "@aptos-connect/keyless-accounts",
 };
 
-export const devnetClient = new Aptos(
-  new AptosConfig({ network: Network.DEVNET })
-);
+export const devnetClient = new Aptos(new AptosConfig({ network: Network.DEVNET }));
 
 /// FIXME: Put your client id here
-export const GOOGLE_CLIENT_ID = "";
+export const GOOGLE_CLIENT_ID =
+  "374353509347-9vhbdjk71ikpl4csvsb9kehclmb8tti5.apps.googleusercontent.com";
